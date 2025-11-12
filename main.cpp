2b336708ef78a6a43d1ea6e9ea422a9273f68e40
JanaOmara <jojojana43@gmail.com>
2025-11-12 15:48:55 +0200

    Refactor animal classes and update main function
    Refactor Animal, Mammal, Bird, Reptile, Enclosure, and Visitor classes. Add display and feed methods for Animal and Mammal. Update main function to demonstrate functionality.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
 #include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-#include <iostream>
-using namespace std;
+Animal::Animal (string n , int a, bool hun){
+    name= n;
+    age=a;
+    isHungry=hun;
+}
 
+void Animal:: display(){
+    cout<< "Name :"<< name<<endl;
+    cout << "Age :"<<age<<endl;
+    cout<<"isHungry :"<<isHungry<<endl;
+}
+void Animal :: feed(){
+    if(isHungry==false){
+        cout<< "not Hungry"<<endl;
+    }
+}
+Mammal:: Mammal(string c,string n, int a ){
+    colour=c;
+}
+void Mammal:: dispaly(){
+    Animal: dispaly();
+    cout<<colour<<endl;
+}
+Bird::Bird (float wing,string n, int a){
+    wingSpan=wing;
+}
+Reptile:: Reptile(bool ven,string n, int a){
+    venomous=ven;
+}
+void Enclosure::addAnimal(Animal* a){
+    a=new Animal[capacity];
+}
+Enclosure::~Enclosure(){
+  delete[] animal;
+}
+void Enclosure:: displayAnimals(){
+    
+}
+Visitor::Visitor(string vn, int t){
+    visitorName=vn;
+    ticketsBought=t;
+}
+void Visitor::dispalyInfo(){
+    cout << "Name "<<visitorName<<endl;
+    cout<<"ticketsBought:"<<ticketsBought<<endl;
+}
+//iknow the rest but not enough time
+int main()
+{
+    Mammal m ("orange ","Lion",5);
+    m.dispaly();
+   Visitor v("mariam",4);
+   v.dispalyInfo();
+   
 
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
     return 0;
 }
