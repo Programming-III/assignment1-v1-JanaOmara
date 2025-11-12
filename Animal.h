91bc529032a7b60e2c7929e6912aa06a6f1ebd25
JanaOmara <jojojana43@gmail.com>
2025-11-12 15:21:53 +0200

    Define Animal class with derived classes
    Added Animal class and derived classes for Mammal, Bird, and Reptile.

--- Animal.h
+++ Animal.h
@@ -1,10 +1,65 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
 
+
+#include <iostream>
 #include <string>
 using namespace std;
+#ifndef ANIMAL_H 
+#define ANIMAL_H
+class Animal{
+    private:
+        string name ;
+        int age ;
+        bool isHungry;
+    public :
+        Animal(){
+            name = "null";
+            age = 0;
+            isHungry= false;
+        }
+        Animal(string n , int a, bool hun);
+        void setname (string n);
+        void setage (int a);
+        string getname ();
+        int getage();
+        void display();
+        void feed();
+};
+class Mammal : public Animal{
+    private:
+        string colour;
+    public:
+        Mammal() : Animal(){
+            colour ="Null";
+        }
+         Mammal (string c,string n, int a ) ;
+     void dispaly();
+};
+class Bird : public Animal{
+    private:
+        float wingSpan;
+    public :
+        Bird() : Animal() {
+            wingSpan= 0;
+        }
+        
+        Bird(float wing,string n, int a);
+         void dispaly();
+    
+};
+class Reptile : public Animal {
+    private:
+     bool venomous ;
+    public:
+    Reptile(): Animal()
+    {
+        venomous = false;
+    }
+    Reptile(bool ven,string n, int a);
+    void dispaly();
+    
+};
+ #endif   
 
-//define animal class here 
 
 
 
