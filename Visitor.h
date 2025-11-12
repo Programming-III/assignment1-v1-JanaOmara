e4b41986cd27b1fc6c9123fcc46ea057d27db491
JanaOmara <jojojana43@gmail.com>
2025-11-12 15:26:59 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,18 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#include <iostream>
 #include <string>
 using namespace std;
-
-//define visitor class here
-
-
-
-
-
-
-
-#endif
+#ifndef VISITOR_H
+#define VISITOR_H
+ class Visitor {
+     private :
+        string visitorName;
+        int ticketsBought ;
+     public :
+     Visitor();
+     Visitor(string vn, int t);
+     void dispalyInfo();
+     
+        
+     
+ };
+ #endif
